8cc383cee159f4bfcf6d4d536d932f42e54d8d8b
Diyan Ali Shaikh <shaikhdiyanali01@gmail.com>
2024-04-05 02:56:59 +0500

    route fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.controllers.face_registration_controller import register_faces
 app = Flask(__name__)
 
 # Routes
-app.add_url_rule('/face-id/create', '/face-id/create', register_faces, methods=['POST'])
+app.add_url_rule('/api/v1/face-id/create', '/api/v1/face-id/create', register_faces, methods=['POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
