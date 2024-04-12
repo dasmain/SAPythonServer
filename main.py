da3f45e84308b61dccc97dce24cd22da65f6d61b
Diyan Ali Shaikh <shaikhdiyanali01@gmail.com>
2024-04-12 21:18:49 +0500

    changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 # Routes
 app.add_url_rule('/api/v1/face-id/create', '/api/v1/face-id/create', register_faces, methods=['POST'])
-app.add_url_rule('/api/v1/face-id/model/face-recognition', '/api/v1/face-id/model/face-recognition', create_model, methods=['POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/controllers/models_controller.py
+++
@@ -1,8 +0,0 @@
-from flask import request, jsonify
-from src.models.face_recognition_model import build_and_train_model
-
-def create_model():
-
-    build_and_train_model('../data/train', '../data/validation')
-
-    return jsonify({'success': True, 'data': {}, 'message': 'Model Creation Complete'}), 200
