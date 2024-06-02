585a377b10a533e744cc63e96c273ad5eeb90e55
Basil <basiljamil1122@gmail.com>
2024-06-02 16:22:23 +0500

    pushing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from src.controllers.face_registration_controller import register_faces
 from src.controllers.face_recognition_controller import recognize_faces
 
 app = Flask(__name__)
+CORS(app)
 
 # Routes
 app.add_url_rule('/api/v1/face-id/create', '/api/v1/face-id/create', register_faces, methods=['POST'])
