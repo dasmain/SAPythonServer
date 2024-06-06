f405d81b1f8ada71e88752b34c20e882b73803d4
Diyan Ali Shaikh <shaikhdiyanali01@gmail.com>
2024-06-06 19:06:11 +0500

    changed to mtcnn

--- src/services/face_recognition_service.py
+++ src/services/face_recognition_service.py
@@ -7,13 +7,12 @@ from scipy.spatial.distance import cosine
 from src.dao.get_faceId import get_all_faceIds
 from src.dao.get_enrolled_students import get_all_students
 from src.services.niqaab_recognition_service import niqab_detection
-from facenet_pytorch import InceptionResnetV1
+from facenet_pytorch import InceptionResnetV1, MTCNN
 import torch
 import base64
 
 load_dotenv()
 
-# MongoDB connection
 mongo_uri = os.getenv("MONGO_URI")
 if not mongo_uri:
     raise ValueError("No MONGO_URI found in environment variables")
@@ -22,9 +21,10 @@ client = pymongo.MongoClient(mongo_uri)
 db = client["sadb"]
 collection = db["face_id"]
 
-# Load the FaceNet model
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+
+mtcnn = MTCNN(keep_all=True, device=device, thresholds=[0.8, 0.9, 0.9])
 
 def preprocess_image(img):
     img = cv2.resize(img, (160, 160))
@@ -68,27 +68,59 @@ def recognize_face(image, recognized, registered_embeddings):
     recognized_ids.append(min_student_id)
     return recognized_ids
 
+# HAAR'S CASCADE ALGORITHM
+
+# def detect_and_recognize_faces(image, filtered_embeddings):
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7, minSize=(30, 30))
+    
+#     recognized_faces = []
+#     recognized_amount = 0
+
+#     for idx, (x, y, w, h) in enumerate(faces):
+#         face = image[y:y+h, x:x+w]
+#         recognized_face_ids = recognize_face(face, recognized_faces, filtered_embeddings)
+#         recognized_faces.extend(recognized_face_ids)
+#         if recognized_face_ids == [None]:
+#             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+#             cv2.putText(image, str(idx+1), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+#         elif recognized_face_ids:
+#             recognized_amount += 1
+#             cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+#             cv2.putText(image, str(idx+1), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+    
+#     return recognized_faces, len(faces), image, recognized_amount
+
+# MTCNN ALGORITHM
+
 def detect_and_recognize_faces(image, filtered_embeddings):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7, minSize=(30, 30))
+
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    boxes, _ = mtcnn.detect(rgb_image)
+    
+    if boxes is None:
+        return [], 0, image, 0
     
     recognized_faces = []
     recognized_amount = 0
 
-    for idx, (x, y, w, h) in enumerate(faces):
-        face = image[y:y+h, x:x+w]
+    for idx, box in enumerate(boxes):
+        x1, y1, x2, y2 = [int(coord) for coord in box]
+        face = image[y1:y2, x1:x2]
         recognized_face_ids = recognize_face(face, recognized_faces, filtered_embeddings)
         recognized_faces.extend(recognized_face_ids)
+        
         if recognized_face_ids == [None]:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.putText(image, str(idx+1), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.putText(image, str(idx+1), (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
         elif recognized_face_ids:
             recognized_amount += 1
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.putText(image, str(idx+1), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.putText(image, str(idx+1), (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
     
-    return recognized_faces, len(faces), image, recognized_amount
+    headcount = len(boxes)
+    return recognized_faces, headcount, image, recognized_amount
 
 def encode_image_to_base64(image):
     _, buffer = cv2.imencode('.jpg', image)
